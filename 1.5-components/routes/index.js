a849cd0b5f09691b5c890e97a00208c2e665e553
Mike Dang <mike.dang@generalassemb.ly>
2017-07-14 11:27:26 -0400

    Organized code

--- 1.5-components/routes/index.js
+++ 1.5-components/routes/index.js
@@ -4,12 +4,12 @@ var Card = require('../models/card');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { });
+  res.render('index', {});
 });
 
 /* GET all the cards */
 router.get('/cards', function(req, res, next) {
-  Card.find({ }, function(err, cards) {
+  Card.find({}, function(err, cards) {
     if (err) console.log(err);
 
     res.json(cards);
@@ -21,14 +21,14 @@ router.post('/cards', function(req, res, next) {
   var question = req.body.question;
 
   var newCard = new Card({
-      question: question
+    question: question
   });
 
   // Save the card
   newCard.save(function(err, card) {
-      if (err) console.log(err);
+    if (err) console.log(err);
 
-      res.json(card);
+    res.json(card);
   });
 });
 
